fd796beb845d94af01246b99421c6d6602e6d483
Deepu <pradeep9100234360@gmail.com>
2025-02-20 17:29:55 +0530

    givenBestExample

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { useEffect } from "react"
 import { useCallback } from "react"
 export default function App(){
   const [title,setTitle] = useState('UseMemoHook')
+  const [count,setCount] = useState(0)
   const users = useMemo(()=>{
     return [
       {
@@ -26,6 +27,28 @@ export default function App(){
       }
      ]
   },[])
+
+  // // Here when ever state get upated the child component geting rendered because of for child,component we are passing reference base array
+  // // so when ever the state updated the refernce of old is overiting to over come this unnecessary renders we go with 
+  // // useMemo Hook That means this useMemo hook set the array reference same until the actual updation done to it.
+  // const users = [
+  //   {
+  //    name:"Sandy",
+  //    details:['age : 23', 'Job-role : solar Technision']
+  //   },
+  //   {
+  //    name:"Deepu",
+  //    details:['age : 23', 'Job-role : Senior Full Stack Developer']
+  //   },
+  //   {
+  //    name:"Lilly",
+  //    details:['age : 23', 'Job-role : Sub Inspector of police']
+  //   },
+  //   {
+  //    name:"Sticker",
+  //    details:['age : 21', 'Job-role : Java Full Stack Developer']
+  //   }
+  //  ]
   const [user,setUserDetails] = useState(users[0].details)
   const userDetails = useCallback(
     (e)=>{
@@ -36,6 +59,9 @@ export default function App(){
   useEffect(()=>{
     console.log('useEffect called')
   },[users,userDetails])
+  function inc(){
+    setCount(()=>count +1)
+  }
   return(
     <div>
       <UserData userData={users} titleName={title} onChnageUser={userDetails}></UserData>
@@ -46,6 +72,8 @@ export default function App(){
           })
         }
       </select>
+      <h2>Counter :: {count}</h2>
+      <button onClick={()=>inc()}>inCreament</button>
     </div>
   )
 }
--- src/UserData.jsx
+++ src/UserData.jsx
@@ -1,7 +1,7 @@
 import { nanoid } from "nanoid"
 import { memo } from "react"
 export default memo(function UserData({titleName,userData,onChnageUser}){
-    console.log(titleName,'this was called')
+    console.log(titleName,'this Child was called')
     return(
         <>
             <h1 style={{textAlign:"center"}}>{titleName}</h1>
